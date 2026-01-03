9d140cf1afa159d4e891387f561c5422710cb191
grishma6 <gxg59150@ucmo.edu>
2026-01-03 15:25:06 -0800

    Richest Customer

--- src/Array.java
+++ src/Array.java
@@ -33,4 +33,34 @@ public class Array {
             System.out.println(numbers[i]);
         }
     }
+
+    public static class TwoDArray {
+        public static void main(String[] args){
+            Scanner scanner = new Scanner(System.in);
+            int rows = scanner.nextInt();
+            int cols = scanner.nextInt();
+
+            int numbers[][] = new int[rows][cols];
+
+            //input
+            //rows
+            for(int i=0; i<rows; i++){
+                //cols
+                for(int j=0; j<cols; j++){
+                    numbers[i][j] = scanner.nextInt();
+                }
+            }
+
+            //output
+            for(int i=0; i<rows; i++){
+                for(int j=0; j<cols; j++){
+                    System.out.print(numbers[i][j] + " ");
+                }
+                System.out.println(
+
+
+                );
+            }
+        }
+    }
 }
