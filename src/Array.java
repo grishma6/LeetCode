1df967d379d9a0a496247eeba4e41ba31a0dd718
grishma6 <gxg59150@ucmo.edu>
2026-01-02 17:55:53 -0800

    array

---
+++ src/Array.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args){
+        //int[] marks = new int[3];
+        //int marks[] = new int[3];// any syntax works above or this declaration is the same
+
+        int marks[] = {95,96,97};
+
+        //marks[0] = 95;
+        //marks[1] = 96;
+        //marks[2] = 97;
+        //System.out.println(marks[0]);
+        //System.out.println(marks[1]);
+        //System.out.println(marks[2]);
+
+        //for(int i=0; i<3; i++){
+        //    System.out.println(marks[i]);
+        //}
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is the size of an array: ");
+        int size = scanner.nextInt();
+        int numbers[] = new int[size];
+
+        //Input
+        for(int i=0; i<size; i++){
+            numbers[i]=scanner.nextInt();
+        }
+
+        //Output
+        for(int i=0; i<size; i++){
+            System.out.println(numbers[i]);
+        }
+    }
+}
