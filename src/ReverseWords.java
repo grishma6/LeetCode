46a22eeee0b950d948215e1915a3a90e0ba144a6
grishma6 <gxg59150@ucmo.edu>
2026-01-05 22:06:25 -0800

    Reverse Words

---
+++ src/ReverseWords.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ReverseWords {
+    public String reverseWords(String s){
+        String[] words =  s.trim().split("\\s+");
+
+        Collections.reverse(Arrays.asList(words));
+
+        return String.join(" ", words);
+    }
+}
