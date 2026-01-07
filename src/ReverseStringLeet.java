d537a6c9c4a53bcf0b4fe0074eb88a060ea215e9
grishma6 <gxg59150@ucmo.edu>
2026-01-06 21:06:01 -0800

    Reverse a String

---
+++ src/ReverseStringLeet.java
@@ -0,0 +1,13 @@
+public class ReverseStringLeet {
+    public void reverse(char[] s){
+        int l=0, r=s.length-1;
+
+        while(l<r){
+            char temp = s[l];
+            s[l]=s[r];
+            s[r]=temp;
+            l++;
+            r--;
+        }
+    }
+}
