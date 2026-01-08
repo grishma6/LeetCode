03ecda6d9d7eda482b6491303c9d0cacf9717fc0
grishma6 <gxg59150@ucmo.edu>
2026-01-07 16:14:12 -0800

    Length of the last word

---
+++ src/LengthofLastWord.java
@@ -0,0 +1,7 @@
+public class LengthofLastWord {
+    public int lengthOfLastWord(String s){
+        String[] words = s.trim().split(" ");
+
+        return words[words.length-1].length();
+    }
+}
