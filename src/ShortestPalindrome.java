c1442340d938bbe84d82814324eea2dcaa52b5b1
grishma6 <gxg59150@ucmo.edu>
2026-01-10 16:12:54 -0800

    shortest-palindrome

---
+++ src/ShortestPalindrome.java
@@ -0,0 +1,12 @@
+public class ShortestPalindrome {
+    public String shortestPalindrome(String s){
+        String rev = new StringBuilder(s).reverse().toString();
+
+        for(int i=0;i<s.length();i++){
+            if(s.startsWith(rev.substring(i))){
+                return rev.substring(0,i) + s;
+            }
+        }
+        return s;
+    }
+}
