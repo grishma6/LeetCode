302aa38e04789c800a2012a98990a471e55d731f
grishma6 <gxg59150@ucmo.edu>
2026-01-14 20:01:05 -0800

    Group Anagrams

---
+++ src/GroupAnagrams.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.HashMap;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] str){
+        //Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for(String s : str){
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
