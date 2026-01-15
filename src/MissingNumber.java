812704509c7cf52066ffe4284090d62e5f23c962
grishma6 <gxg59150@ucmo.edu>
2026-01-14 18:32:08 -0800

    Missing Number

--- src/MissingNumber.java
+++ src/MissingNumber.java
@@ -2,6 +2,7 @@ import java.util.HashSet;
 
 public class MissingNumber {
     public int missingNumber(int[] nums){
+        //Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
         HashSet<Integer> set = new HashSet<>();
 
         for(int n : nums){
