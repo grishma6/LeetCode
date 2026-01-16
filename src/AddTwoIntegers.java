0d1d5104653f2c79aa4a68ff60133cca3ab316e1
grishma6 <gxg59150@ucmo.edu>
2026-01-16 14:43:20 -0800

    add-two-integers

---
+++ src/AddTwoIntegers.java
@@ -0,0 +1,6 @@
+public class AddTwoIntegers {
+    public int addTwoIntegers(int num1, int num2){
+        //Given two integers num1 and num2, return the sum of the two integers.
+        return num1+num2;
+    }
+}
