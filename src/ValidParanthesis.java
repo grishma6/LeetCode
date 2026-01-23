aa5cbb0d5d83f7bebf5d969d32bdce8378c814c3
grishma6 <gxg59150@ucmo.edu>
2026-01-22 19:29:11 -0800

    valid-parentheses

---
+++ src/ValidParanthesis.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class ValidParanthesis {
+    public boolean validParanthesis(String s){
+        //Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+        //
+        //An input string is valid if:
+        //
+        //Open brackets must be closed by the same type of brackets.
+        //Open brackets must be closed in the correct order.
+        //Every close bracket has a corresponding open bracket of the same type.
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()) return false;
+
+                char top = stack.pop();
+
+                if(ch == ')' && top != '(') return false;
+                if(ch == '}' && top != '{') return false;
+                if(ch == ']' && top != '[') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
