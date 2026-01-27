7bf069e8ddf7e9ea72b3f1a26a15373b8ccd6353
grishma6 <gxg59150@ucmo.edu>
2026-01-26 19:37:08 -0800

    binary search LeetCode

---
+++ src/BinarSearchLeet.java
@@ -0,0 +1,20 @@
+public class BinarSearchLeet {
+    public int binarySearchLeet(int[] nums, int target){
+        return binarySearch(nums, target);
+    }
+
+    public int binarySearch(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
+
+        while(low <= high){
+            int middle = low + (high-low)/2;
+            int value = nums[middle];
+
+            if(value<target) low = middle+1;
+            else if(value>target) high = middle-1;
+            else return middle;
+        }
+        return -1;
+    }
+}
