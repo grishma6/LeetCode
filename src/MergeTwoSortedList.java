763f78fe82746c0f7e535beafcd1ba759b96b7ec
grishma6 <gxg59150@ucmo.edu>
2026-01-27 21:55:30 -0800

    merge-two-sorted-lists

---
+++ src/MergeTwoSortedList.java
@@ -0,0 +1,34 @@
+public class MergeTwoSortedList {
+    public ListNode mergetwoList(ListNode list1, ListNode list2){
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        while(list1 != null && list2 != null){
+            if(list1.val<=list2.val){
+                tail.next = list1;
+                list1 = list1.next;
+            }else{
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        if(list1 != null){
+            tail.next = list1;
+        }else{
+            tail.next = list2;
+        }
+        return dummy.next;
+    }
+
+    class ListNode{
+        int val;
+        ListNode next;
+
+        ListNode(int val){
+            this.val = val;
+            this.next = null;
+        }
+    }
+}
