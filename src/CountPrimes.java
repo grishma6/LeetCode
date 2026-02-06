192c1ba5a7edcde3b454c370a133612b4fe94c15
grishma6 <gxg59150@ucmo.edu>
2026-02-05 21:37:32 -0800

    count-primes

---
+++ src/CountPrimes.java
@@ -0,0 +1,28 @@
+public class CountPrimes {
+    public int countPrimes(int n){
+        if(n<=2){
+            return 0;
+        }
+        boolean[] isPrime = new boolean[n];
+        for(int i=2; i<n; i++){
+            isPrime[i] = true;
+        }
+
+        // mark multiples as not prime
+        for(int i=2; i*i<n; i++) {
+            if (isPrime[i]) {
+                // start from i*i because smaller multiples already marked
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+            int count = 0;
+            for(int i=0;i<n;i++){
+                if(isPrime[i]){
+                    count++;
+                }
+            }
+        return count;
+    }
+}
