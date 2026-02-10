f7288d80e8389ea03028caa09411d8324f2ea894
grishma6 <gxg59150@ucmo.edu>
2026-02-09 19:31:43 -0800

    max-consecutive-ones

---
+++ src/MaximumConsecutiveOnes.java
@@ -0,0 +1,16 @@
+public class MaximumConsecutiveOnes {
+    public int maximumConsecutiveOnes(int[] nums){
+        int count = 0;
+        int maxCount = 0;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 1){
+                count++;
+                maxCount = Math.max(maxCount, count);
+            }else{
+                count = 0;
+            }
+        }
+        return maxCount;
+    }
+}
